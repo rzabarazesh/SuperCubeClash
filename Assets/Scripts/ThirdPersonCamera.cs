2a305d393320786a88c0adffd718a63b0af4d817
Vaibhav Vishal <vvishal@usc.edu>
2017-06-04 20:57:00 -0700

    Camera Rotation
    Added features for camera rotation, tested for Laptop Mouse Controls. Touch features need to be coded and tested later.

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,43 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+	private const float MIN_DIST = 2.0f;
+	private const float MAX_DIST = 12.0f;
+
+	public Transform lookAt;
+	public Transform camTransform;
+
+	private Camera cam;
+
+	private float distance = 5.0f;
+	private float currentX = 0.0f;
+	private float currentY = 0.0f;
+	private float sensitivityX = 4.0f;
+	private float sensitivityY = 1.0f;
+
+	private void Start()
+	{
+		camTransform = transform;
+		cam = Camera.main;
+	}
+
+	private void Update()
+	{
+		currentX += Input.GetAxis("Mouse X");
+		currentY += Input.GetAxis("Mouse Y");
+		distance += Input.GetAxis("Mouse ScrollWheel");
+		distance = Mathf.Clamp(distance, MIN_DIST, MAX_DIST);
+
+	}
+
+	private void LateUpdate()
+	{
+		Vector3 dir = new Vector3(0, 0, -distance);
+		Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+		camTransform.position = lookAt.position + rotation * dir;
+		camTransform.LookAt(lookAt.position);
+	}
+}
