32a4c048d87179b770439ffbddc712e6e8edfdb4
Vaibhav Vishal <vvishal@usc.edu>
2017-06-08 17:55:54 -0700

    Clamp Camera Rotation
    [VV] Fixed the issue of switching faces

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -15,8 +15,13 @@ public class ThirdPersonCamera : MonoBehaviour
 	private float distance = 5.0f;
 	private float currentX = 0.0f;
 	private float currentY = 0.0f;
-	private float sensitivityX = 4.0f;
-	private float sensitivityY = 1.0f;
+	//private float sensitivityX = 4.0f;
+	//private float sensitivityY = 1.0f;
+
+	public float yLimit = 60.0f;
+
+	public float perspectiveZoomSpeed = 0.5f;
+	public float orthoZoomSpeed = 0.5f;
 
 	private void Start()
 	{
@@ -26,10 +31,35 @@ public class ThirdPersonCamera : MonoBehaviour
 
 	private void Update()
 	{
-		currentX += Input.GetAxis("Mouse X");
+		if (Input.touchCount == 2)
+		{
+			Touch touchZero = Input.GetTouch(0);
+			Touch touchOne = Input.GetTouch(1);
+
+			Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+			Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+			float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+			float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+			float deltaMagnitudediff = prevTouchDeltaMag - touchDeltaMag;
+
+			if (cam.orthographic)
+			{
+				cam.orthographicSize += deltaMagnitudediff * orthoZoomSpeed;
+				cam.orthographicSize = Mathf.Max(cam.orthographicSize, 0.1f);
+			}
+			else
+			{
+				cam.fieldOfView += deltaMagnitudediff * perspectiveZoomSpeed;
+				cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, 0.1f, 179.9f);
+			}
+		}
+        currentX += Input.GetAxis("Mouse X");
 		currentY += Input.GetAxis("Mouse Y");
 		distance += Input.GetAxis("Mouse ScrollWheel");
 		distance = Mathf.Clamp(distance, MIN_DIST, MAX_DIST);
+        currentY = Mathf.Clamp(currentY, -yLimit, yLimit);
 
 	}
 
